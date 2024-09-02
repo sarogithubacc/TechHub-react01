ab80de4cf814b7f0cb74eb3ee7850ee3d79fd76f
S H A N M U G A M . J <146160718+sarogithubacc@users.noreply.github.com>
2024-09-02 23:06:01 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -5,7 +5,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchData = async () => {
-    await fetch('http://localhost:4000/allproducts')
+    await fetch('https://techhub-react01.onrender.com/allproducts')
       .then((res) => res.json())
       .then((data) => {
         setAllProducts(data);
@@ -16,7 +16,7 @@ const ListProduct = () => {
     fetchData();
   }, []);
   const removeProduct = async (id) => {
-    await fetch('http://localhost:4000/removeproduct',{
+    await fetch('https://techhub-react01.onrender.com/removeproduct',{
      method:'POST',
      headers:{
       Accept:'Application/json',
